0035a34f100f4b73df2aa9cb41c4da4928b349e2
kalkine-rahul <rahul.kumar1@kalkine.co.in>
2025-11-03 12:29:43 +0530

    Update padding in Screener component
    Adjusted padding in the main container.

--- src/app/screeners/page.js
+++ src/app/screeners/page.js
@@ -7,10 +7,11 @@ import Screeners from "../../component/ScreenerPage";
 export default function Screener() {
  
   return (
-  <div className="pt-24 px-4 md:px-8 min-h-screen"> 
+  <div className="pt-12 px-4 md:px-8 min-h-screen"> 
       <div className="mb-8">
     <Screeners/>
     </div>
        </div>
   );
 }
+
